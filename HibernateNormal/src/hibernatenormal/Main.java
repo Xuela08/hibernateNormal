02ffb16c7089d46c3cfbabfcee8b6342b2619b14
Manu <mjcd0005@gmail.com>
2015-04-22 12:10:17 +0200

    Añadido un cambio

--- HibernateNormal/src/hibernatenormal/Main.java
+++ HibernateNormal/src/hibernatenormal/Main.java
@@ -4,7 +4,7 @@ package hibernatenormal;
 public class Main {
     
     public static void main(String[] args) {
-        // Estamos en la rama trabajo
+        // Estamos en la rama trabajo de verdad
     }
     
 }
