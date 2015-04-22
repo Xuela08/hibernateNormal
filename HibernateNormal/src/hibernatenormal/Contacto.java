205d2816181c1c4f8a70968d59d8eb807877a2e2
Manu <mjcd0005@gmail.com>
2015-04-22 12:27:08 +0200

    Se ha creado la persistencia con hibernate

---
+++ HibernateNormal/src/hibernatenormal/Contacto.java
@@ -0,0 +1,58 @@
+package hibernatenormal;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Contacto implements Serializable {
+    private long id;
+    private String nombre;
+    private String email;
+    private String telefono;
+
+    public Contacto() {
+    }
+
+    public Contacto(long id, String nombre, String email, String telefono) {
+        this.id = id;
+        this.nombre = nombre;
+        this.email = email;
+        this.telefono = telefono;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    private void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+    
+}
+
--- HibernateNormal/src/hibernatenormal/Main.java
+++ HibernateNormal/src/hibernatenormal/Main.java
@@ -1,10 +1,47 @@
 package hibernatenormal;
 
+import java.util.List;
+
+
+
 
 public class Main {
     
     public static void main(String[] args) {
-        // Estamos en la rama trabajo de verdad
+        ContactosDAO contactosDAO = new ContactosDAO(); 
+        Contacto contactoRecuperado = null;  
+        long idAEliminar = 0;  
+
+        //Creamos tes instancias de Contacto 
+        Contacto contacto1 = new Contacto(0, "Contacto 1", "contacto1@contacto.com", "12345678"); 
+        Contacto contacto2 = new Contacto(0, "Contacto 2", "contacto2@contacto.com", "87654321"); 
+        Contacto contacto3 = new Contacto(0, "Contacto 3", "contacto3@contacto.com", "45612378");  
+
+        //Guardamos las tres instancias, guardamos el id del contacto1 para usarlo posteriormente 
+        idAEliminar = contactosDAO.guardaContacto(contacto1); 
+        contactosDAO.guardaContacto(contacto2); 
+        contactosDAO.guardaContacto(contacto3);  
+
+        //Modificamos el contacto 2 y lo actualizamos 
+        contacto2.setNombre("Nuevo Contacto 2"); 
+        contactosDAO.actualizaContacto(contacto2);  
+
+        //Recuperamos el contacto1 de la base de datos 
+        contactoRecuperado = contactosDAO.obtenContacto(idAEliminar); 
+        System.out.println("Recuperamos a " + contactoRecuperado.getNombre());  
+
+        //Eliminamos al contactoRecuperado (que es el contacto3) 
+        contactosDAO.eliminaContacto(contactoRecuperado);  
+
+        //Obtenemos la lista de contactos que quedan en la base de datos y la mostramos 
+        List<Contacto> listaContactos = contactosDAO.obtenListaContactos();  
+        System.out.println("Hay " + listaContactos.size() + "contactos en la base de datos");  
+
+        for(Contacto c : listaContactos) 
+        { 
+            System.out.println("-> " + c.getNombre()); 
+        }
     }
     
 }
+
